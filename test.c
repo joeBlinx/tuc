39214594dbffd0d91e97bb4361855e1d219e483f
StivenAigle <saigle@unistra.fr>
2022-03-27 09:24:39 +0200

    add files comming from my C courses

---
+++ test.c
@@ -0,0 +1,106 @@
+#include "test.h"
+
+void print_int_int(int a, char const *op, int b) {
+  printf("%d %s %d\n", a, op, b);
+}
+void print_size_t_size_t(size_t a, char const *op, size_t b) {
+  printf("%ld %s %ld\n", a, op, b);
+}
+void print_int_size_t(int a, char const *op, size_t b) {
+  printf("%d %s %ld\n", a, op, b);
+}
+void print_size_t_int(size_t a, char const *op, int b) {
+  printf("%ld %s %d\n", a, op, b);
+}
+struct Pointer {
+  size_t allocated;
+  void *ptr;
+};
+struct PointersStore {
+  int index_max;
+  struct Pointer pointers[100];
+  size_t bytes_allocated;
+};
+static struct PointersStore pointers_store = {.index_max = 0,
+                                              .bytes_allocated = 0};
+int test_passed = 0;
+int test_failed = 0;
+static int nb_malloc = 0;
+static int nb_free = 0;
+TestFunction unit_test[100] = {0};
+int number_test = 0;
+static size_t bytes_allocated = 0;
+static void add_new_pointer(void *ptr, size_t bytes) {
+  pointers_store.bytes_allocated += bytes;
+  pointers_store.pointers[pointers_store.index_max++] =
+      (struct Pointer){.allocated = bytes, .ptr = ptr};
+  if (bytes == 1024) {
+    int a = 0;
+    int b = a;
+  }
+}
+static void remove_pointer(void *ptr) {
+  int index = 0;
+  while (index < pointers_store.index_max &&
+         ptr != pointers_store.pointers[index].ptr) {
+    index++;
+  }
+  if (index == pointers_store.index_max) {
+    return;
+  }
+  pointers_store.bytes_allocated -= pointers_store.pointers[index].allocated;
+  for (int i = index; i < pointers_store.index_max; i++) {
+    pointers_store.pointers[i] = pointers_store.pointers[i + 1];
+  }
+}
+#ifdef __GNUC__
+static bool use_custom = true;
+extern void *__libc_malloc(size_t size);
+extern void __libc_free(void *ptr);
+void *my_malloc(size_t bytes) {
+  static bool first_time = true;
+  if (first_time) {
+    use_custom = false;
+    puts("Beginning of Test");
+    use_custom = true;
+    first_time = false;
+  }
+  void *ptr = __libc_malloc(bytes);
+  add_new_pointer(ptr, bytes);
+  nb_malloc += 1;
+  return ptr;
+}
+void *malloc(size_t bytes) {
+  if (use_custom) {
+    return my_malloc(bytes);
+  }
+  return __libc_malloc(bytes);
+}
+void free(void *ptr) {
+  remove_pointer(ptr);
+  nb_free += 1;
+  __libc_free(ptr);
+}
+#endif
+TEST(MemCheck) {
+  REQUIRE_OP(nb_malloc, ==, nb_free, "Nb Free %d, Nb Malloc %d\n", nb_free, nb_malloc);
+  REQUIRE_OP(pointers_store.bytes_allocated, ==, 0,"Memory not freed %ld\n", pointers_store.bytes_allocated);
+}
+
+bool run_all_test() {
+  free(malloc(1)); // here to force the alloc of stdout buffer, thus it will not count in nb_alloc and nb_free
+  REGISTER(MemCheck);
+  for (int test = 0; test < number_test; test++) {
+    int failed = test_failed;
+    unit_test[test].test(unit_test[test].name);
+    if (failed == test_failed) {
+      test_passed += 1;
+    } else {
+    }
+  }
+  print("==========SUMMARY===========\n");
+  printf("Test Launched %d\n", number_test);
+  print(GREEN "Test Succeeded %d\n", test_passed);
+  print(RED "Test Failed %d\n", test_failed);
+  return test_failed;
+}
---
+++ test.h
@@ -0,0 +1,81 @@
+/**
+ * ATTENTION: Vous n'avez en aucun cas le droit de toucher à ce fichier.
+ * Une modification de ce ficher sera sanctionné par une note nulle !
+ */
+#ifndef TEST_HEADER
+#define TEST_HEADER
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+void print_int_int(int a, char const *op, int b);
+void print_size_t_size_t(size_t a, char const *op, size_t b);
+void print_int_size_t(int a, char const *op, size_t b);
+void print_size_t_int(size_t a, char const *op, int b);
+void print_int_int(int a, char const *op, int b);
+typedef struct TestFunction {
+  void (*test)();
+  char *name;
+} TestFunction;
+extern int test_passed;
+extern int test_failed;
+extern TestFunction unit_test[100];
+extern int number_test;
+bool run_all_test();
+#define COLOR "\033["
+#define RED COLOR "31m"
+#define GREEN COLOR "32m"
+#define DEFAULT COLOR "0m"
+#define print(...)                                                             \
+  do {                                                                         \
+    printf(__VA_ARGS__);                                                       \
+    fputs(DEFAULT, stdout);                                                    \
+  } while (0)
+#define TEST(A) void test##A(const char *test_name)
+
+#define GET_N_ARGS(_1, _2, _3, _4, _5, N, ...) N
+#define COUNT_ARGS(...) GET_N_ARGS(__VA_ARGS__, 5, 4, 3, 2, 1)
+#define STRINGIFY(A) #A
+#define OVERRIDE(_1, _2, _3, _4, N, ...) N
+#define REGISTER1(A)                                                           \
+  unit_test[number_test++] = (TestFunction) {                                  \
+    .test = test##A, .name = STRINGIFY(A)                                      \
+  }
+#define REGISTER2(A, B)                                                        \
+  REGISTER1(A);                                                                \
+  REGISTER1(B)
+#define REGISTER3(A, B, C)                                                     \
+  REGISTER2(A, B);                                                             \
+  REGISTER1(C)
+#define REGISTER4(A, B, C, D)                                                  \
+  REGISTER3(A, B, C);                                                          \
+  REGISTER1(D)
+
+#define REGISTER(...)                                                          \
+  do {                                                                         \
+    OVERRIDE(__VA_ARGS__, REGISTER4, REGISTER3, REGISTER2, REGISTER1)          \
+    (__VA_ARGS__);                                                             \
+  } while (0)
+  static void dummy_print(int a, char const* b, int c){}
+#define _REQUIRE(COND, PrintFunction, A, Op, B, ...)                           \
+  do {                                                                         \
+    if (!(COND)) {                                                             \
+      print(RED "===== Test %s failed =====\n", test_name);                    \
+      printf("condition " RED #COND DEFAULT " not passed, %s:%d\n", __FILE__,    \
+             __LINE__);                                                        \
+      if (strcmp(Op, "")) {                                                    \
+        PrintFunction(A, Op, B);                                               \
+      }                                                                        \
+      print(""__VA_ARGS__);                                                    \
+      puts("==========================================");                      \
+      test_failed += 1;                                                        \
+      return;                                                                  \
+    }                                                                          \
+  } while (0)
+#define REQUIRE(COND, ...) _REQUIRE(COND, dummy_print, 0, "", 0, __VA_ARGS__)
+#define PrintOp(A, B) _Generic((A), \
+		int : _Generic((B), int : print_int_int, size_t: print_int_size_t), \
+		size_t: _Generic((B), size_t: print_size_t_size_t, int: print_size_t_int) \
+		)
+#define REQUIRE_OP(A, Op, B, ...)                                              \
+  _REQUIRE((A Op B), PrintOp(A, B), A, #Op, B, __VA_ARGS__);
+#endif
