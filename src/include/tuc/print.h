9cb2522d46ad116a67b446a9a1a374c1148d8b40
JoeBlinx <aiglestiven@zohomail.eu>
2022-04-02 14:03:04 +0200

    reorganize print declaration

--- src/include/tuc/print.h
+++ src/include/tuc/print.h
@@ -10,15 +10,15 @@
     printf(__VA_ARGS__);                                                       \
     fputs(DEFAULT, stdout);                                                    \
   } while (0)
-TUC_EXPORT void print_intint(int a, char const *op, int b);
-TUC_EXPORT void print_size_tsize_t(size_t a, char const *op, size_t b);
-TUC_EXPORT void print_intsize_t(int a, char const *op, size_t b);
-TUC_EXPORT void print_size_tint(size_t a, char const *op, int b);
-TUC_EXPORT void print_doublesize_t(double a, char const *op, size_t b);
-TUC_EXPORT void print_size_tdouble(size_t a, char const *op, double b);
-TUC_EXPORT void print_intdouble(int a, char const *op, double b);
-TUC_EXPORT void print_doubleint(double a, char const *op, int b);
-TUC_EXPORT void print_doubledouble(double a, char const *op, double b);
+TUC_EXPORT void print_intint(int a, char const* op, int b);
+TUC_EXPORT void print_intsize_t(int a, char const* op, size_t b);
+TUC_EXPORT void print_intdouble(int a, char const* op, double b);
+TUC_EXPORT void print_size_tsize_t(size_t a, char const* op, size_t b);
+TUC_EXPORT void print_size_tint(size_t a, char const* op, int b);
+TUC_EXPORT void print_size_tdouble(size_t a, char const* op, double b);
+TUC_EXPORT void print_doublesize_t(double a, char const* op, size_t b);
+TUC_EXPORT void print_doubleint(double a, char const* op, int b);
+TUC_EXPORT void print_doubledouble(double a, char const* op, double b);
 
 #define GET_N_ARGS(_1, _2, _3, _4, _5, N, ...) N
 #define COUNT_ARGS(...) GET_N_ARGS(__VA_ARGS__, 5, 4, 3, 2, 1)
